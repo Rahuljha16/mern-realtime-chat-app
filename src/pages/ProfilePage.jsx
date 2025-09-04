a677e29f43e29a74b24d1f8a3b7f4434791a40c0
Rahuljha16 <rahuljha7136@gmail.com>
2025-09-04 12:02:22 +0530

    Updated project files

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -9,6 +9,11 @@ const ProfilePage = () => {
   const [ name, setName] = useState("Martin johnson")
   const [bio,setBio] = useState("Hi Everyone, I am using SnapTalk")
 
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    navigate('/')
+  }
+
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
         <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 items-center justify-between max-sm:flex-col-reverse rounded-lg'>
@@ -19,9 +24,13 @@ const ProfilePage = () => {
               <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}/>
               upload profile image
             </label>
-            <input type="text" />
+            <input  onChange={(e)=>setName(e.target.value)} value={name}
+            type="text" required placeholder='Your Name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'/>
+            <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
+            placeholder='Write profile bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500' rows={4}></textarea>
+
+            <button type="submit" className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
           </form>
-          <img src="" alt="" />
         </div>
     </div>
   )
